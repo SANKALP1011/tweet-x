e5b3430a1bd4f12e791c27bc563dab117b42db01
SANKALP1011 <tigerishan426@gmail.com>
2022-05-25 14:55:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+
+function App() {
+  return <>
+  
+  <h1>This is the tweet x ui.</h1>
+  
+  </>
+}
+
+export default App;
